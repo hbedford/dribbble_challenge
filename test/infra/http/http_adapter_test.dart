bb7305840d5fc6b941c15217d9af9ba589873ade
hugo bedford <hugo.bedford@gmail.com>
2021-03-10 12:26:56 -0300

    ensure  httpAdapter returns null if get returns 204

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -102,5 +102,12 @@ void main() {
 
       expect(response, null);
     });
+    test('Should return null if get returns 204', () async {
+      mockResponse(204, body: '');
+
+      final response = await sut.request(url: url, method: 'get');
+
+      expect(response, null);
+    });
   });
 }
