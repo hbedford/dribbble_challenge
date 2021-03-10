ed6487d0fbc8e5ab950fdedb79e3d45de29a2034
hugo <hugo.bedford@gmail.com>
2021-03-10 19:01:04 -0300

    Ensure when click EnterButton the LoadingWidget show & Enter Button hide

--- test/login/login_widgets_test.dart
+++ test/login/login_widgets_test.dart
@@ -1,4 +1,5 @@
 import 'package:dribbble_challenge/infra/injections.dart';
+import 'package:dribbble_challenge/ui/screens/login/login_controller.dart';
 import 'package:dribbble_challenge/ui/screens/login/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -21,5 +22,14 @@ void main() {
     expect(buttonLogin, findsOneWidget);
 
     expect(find.byType(CircularProgressIndicator), findsNothing);
+  });
+  testWidgets(
+      'Ensure when you click on EnterButton the CircularProgressIndication turn on and Enter Button turn off',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(loginPage);
+    injection.get<LoginController>().changeLoading(true);
+    await tester.pump();
+    expect(buttonLogin, findsNothing);
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
   });
 }
