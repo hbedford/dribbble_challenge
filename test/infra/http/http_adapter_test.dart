b3c24d36e1d3963a4e1f943a82773eebf78f003a
hugo bedford <hugo.bedford@gmail.com>
2021-03-10 12:29:10 -0300

    ensure httpAdapter returns BadRequestError if get returns 400

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -109,5 +109,12 @@ void main() {
 
       expect(response, null);
     });
+    test('Should return BadRequestError if get returns 400', () async {
+      mockResponse(400, body: '');
+
+      final future = sut.request(url: url, method: 'get');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
   });
 }
