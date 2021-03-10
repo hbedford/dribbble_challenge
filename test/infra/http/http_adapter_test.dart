a727a226b30d227eaae4f793e77b6acab413b469
hugo bedford <hugo.bedford@gmail.com>
2021-03-10 11:44:51 -0300

    ensure httpAdapter returns ServerError if method passed is invalid

---
+++ test/infra/http/http_adapter_test.dart
@@ -0,0 +1,33 @@
+import 'package:dribbble_challenge/infra/http/http_adapter.dart';
+import 'package:http/http.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+import 'package:faker/faker.dart';
+
+class ClientMock extends Mock implements Client {}
+
+void main() {
+  HttpAdapter sut;
+  ClientMock client;
+  String url;
+  setUp(() {
+    client = ClientMock();
+    sut = HttpAdapter(client);
+    url = faker.internet.httpUrl();
+  });
+  group('Method post', () {
+    PostExpectation mockRequest() => when(
+        client.post(any, body: anyNamed('body'), headers: anyNamed('headers')));
+    void mockResponse(int statusCode,
+            {String body = '{"any_key":"any_value"}'}) =>
+        mockRequest().thenAnswer((_) async => Response(body, statusCode));
+    void mockError() => mockRequest().thenThrow(Exception());
+    setUp(() {
+      mockResponse(200);
+    });
+    test('Should throw ServerError if method passed is invalid ', () {
+      final future = sut.request(url: url, method: 'invalid');
+      expect(future, throwsA(HttpError.serverError));
+    });
+  });
+}
